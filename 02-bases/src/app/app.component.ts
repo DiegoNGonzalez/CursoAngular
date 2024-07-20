3a3da0335acd25bd3555f2345fa3461084a63bf5
Diego Gonzalez <diego.gonzalez2@alumnos.frgp.utn.edu.ar>
2024-07-20 19:18:27 -0300

    counter

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+
 })
 export class AppComponent {
-  title = 'bases';
+  public title:string = 'First angular app';
+  public counter:number=10;
+
+  add1(value:number):void{
+    this.counter +=value;
+  }
+  resetCounter():void{
+    this.counter=10;
+  }
 }
