27ab9422614bf03f45014f9041c8323ca68acbda
Diego Gonzalez <diego.gonzalez2@alumnos.frgp.utn.edu.ar>
2024-07-27 17:01:07 -0300

    oneway databiding

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  public name:string ='ironman';
+  public age:number=45;
 
+  get capitalizeName():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`
+  }
+
+  changeHero():void{
+    this.name='Spiderman';
+  }
+
+  changeAge():void{
+    this.age=25;
+  }
 }
