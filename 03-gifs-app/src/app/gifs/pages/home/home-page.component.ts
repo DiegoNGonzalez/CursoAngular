187fec4985f00fdda4c7965b6503222d158c6bf9
Diego Gonzalez <diego.gonzalez2@alumnos.frgp.utn.edu.ar>
2024-07-28 15:25:29 -0300

    diseñando gift component

---
+++ 03-gifs-app/src/app/gifs/pages/home/home-page.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import {  Component } from '@angular/core';
+
+@Component({
+    selector: 'gift-home-page',
+    templateUrl: './home-page.component.html',
+
+})
+export class HomePageComponent { }
