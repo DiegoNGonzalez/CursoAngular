dbcb448aa8f424b2143289e7f249c36690adb439
Diego Gonzalez <diego.gonzalez2@alumnos.frgp.utn.edu.ar>
2024-07-28 00:53:14 -0300

    flujo completo

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -9,9 +9,20 @@ import { Character } from '../../interfaces/character.interface';
 })
 export class ListComponent {
 
+
+
   @Input()
   public characterList:Character[]=[{
     name:'Trunks',
     power:10
   }]
+
+  @Output()
+  public onDelete:EventEmitter<number>= new EventEmitter();
+
+
+  onDeleteCharacter(index:number):void{
+    console.log({index})
+    this.onDelete.emit(index);
+  }
 }
