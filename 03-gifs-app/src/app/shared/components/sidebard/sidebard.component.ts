ba13a5f271558fc94b875a633db344e3affcbf0f
Diego Gonzalez <diego.gonzalez2@alumnos.frgp.utn.edu.ar>
2024-07-28 14:11:43 -0300

    diseño y estructura principal GifsApp

---
+++ 03-gifs-app/src/app/shared/components/sidebard/sidebard.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'share-sidebard',
+    templateUrl: './sidebard.component.html',
+    styleUrl: './sidebard.component.css',
+
+})
+export class SidebardComponent { }
