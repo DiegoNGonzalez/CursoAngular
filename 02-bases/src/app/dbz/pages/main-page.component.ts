75ac34d80facdc23e5917c1228d8a10d6b0380b1
Diego Gonzalez <diego.gonzalez2@alumnos.frgp.utn.edu.ar>
2024-07-28 10:33:37 -0300

    servicios

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,3 +1,4 @@
+import { DbzService } from '../services/dbz.service';
 import { Character } from './../interfaces/character.interface';
 import { Component } from '@angular/core';
 
@@ -7,22 +8,8 @@ import { Component } from '@angular/core';
 })
 
 export class MainPageComponent {
-  public characters: Character[]=[{
-    name:'Krillin',
-    power:1000
-  },{
-    name:'Goku',
-    power:9500
-  },{
-    name:'Vegeta',
-    power:7500
-  }];
-
-  onNewCharacter(character:Character):void{
-    this.characters.push(character);
+  constructor(public dbzService:DbzService){
+  
   }
 
-  onDeleteCharacter(index:number){
-    this.characters.splice(index,1);
-  }
 }
